c3660489c18e3f9171838884693f57318219b2e7
ZHCode <admin@zh-code.com>
2020-05-10 13:28:54 -0400

    Add verify test

--- simulate_test.go
+++ simulate_test.go
@@ -1,6 +1,8 @@
 package hilowsimulator
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSimulate(t *testing.T) {
 	config := &Configuration{
@@ -27,5 +29,20 @@ func TestSimulate(t *testing.T) {
 		t.Error(err)
 	} else if len(ret) != 100 {
 		t.Error("Invalid amount of simulations returned")
+	}
+}
+
+func TestVerify(t *testing.T) {
+	serverSeed := "71ef42d840a9bb600f5e57dc22acb07f5675eae0c7b52c18db2f1fa42ac92b0a"
+	clientSeed := "e3e154b55fe29671ca2f48b7dd58210b38cfae7f168e6639b350767f8c2f58ad"
+	nonce := uint64(0)
+	rollNum := float64(24.88)
+
+	valid, err := Verify(clientSeed, serverSeed, nonce, rollNum)
+
+	if err != nil {
+		t.Error(err)
+	} else if !valid {
+		t.Error("Result invalid")
 	}
 }
